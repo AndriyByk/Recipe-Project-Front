3541d395d914c4d827c4755cf34a59a561de0d1b
AndriyByk <andrew.byk.t@gmail.com>
2023-04-11 16:46:12 +0300

    repairing

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.get('/*', (req,res) => {
   res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 });
 
-app.get('/*', (req,res) => {
-  res.sendFile('index.html', {root: '<name-of-app>'});
-});
+// app.get('/*', (req,res) => {
+//   res.sendFile('index.html', {root: '<name-of-app>'});
+// });
 
 app.listen(process.env.PORT || 8080);
