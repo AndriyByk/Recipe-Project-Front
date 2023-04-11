06a493e313579a235f0b1bb5ffbaf678e8eab430
AndriyByk <andrew.byk.t@gmail.com>
2023-04-11 17:43:09 +0300

    repairing

--- server.js
+++ server.js
@@ -5,19 +5,12 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
-console.log(__dirname);
-console.log(__dirname + '/dist/<name-of-app>');
+app.get('/*', (req,res) => {
+  res.sendFile(path.join(__dirname + '/dist/<name-of-app>/index.html'));
+});
 
 app.get('/favicon.ico', (req, res) => {
   res.sendFile(path.resolve(__dirname, '/dist/<name-of-app>/favicon.ico'));
 });
-
-app.get('/*', (req,res) => {
-  res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
-});
-
-// app.get('/*', (req,res) => {
-//   res.sendFile('index.html', {root: '<name-of-app>'});
-// });
 
 app.listen(process.env.PORT || 8080);
