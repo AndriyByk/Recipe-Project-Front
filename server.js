3f478082b80a3c24f2d634323760a27ebf3edc85
AndriyByk <andrew.byk.t@gmail.com>
2023-04-11 16:00:27 +0300

    repairing

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
-app.get('/*', function(req,res) {
+app.get('/favicon.ico', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '/dist/<name-of-app>/favicon.ico'));
+});
 
+app.get('/*', (req,res) => {
   res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 });
 
